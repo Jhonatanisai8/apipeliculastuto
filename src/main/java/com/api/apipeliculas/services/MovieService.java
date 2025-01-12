892462cd56e38d91543c86bc30a982704e51dad0
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-12 14:39:34 -0500

    clase movie-service: implementacion de los metodos para obtener  todas las peliculas y ademas para obtener una pelicula por su ID

---
+++ src/main/java/com/api/apipeliculas/services/MovieService.java
@@ -0,0 +1,26 @@
+package com.api.apipeliculas.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.apipeliculas.models.Movie;
+import com.api.apipeliculas.repository.MovieRepository;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+    public List<Movie> getAllMovies() {
+        return repository.findAll();
+    }
+
+    public Movie getMovieById(Long id) {
+        return repository
+                .findById(id).get();
+    }
+
+}
