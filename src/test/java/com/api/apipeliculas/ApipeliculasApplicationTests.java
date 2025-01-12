19efeddf4f10c1dad60eb6c7cbc9d07166561331
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-12 13:02:32 -0500

    configuracion inicial del proyecto y ademas de la base de datos

---
+++ src/test/java/com/api/apipeliculas/ApipeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apipeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
