53be3f434f519f4f73883fc4f9cded877cbb4aff
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-12 14:44:34 -0500

    controlador de Pelicula: implementacion de los metodos para listar todas las peliculas ademas de obtener una pelicula por su ID (GET-MAPPING)

---
+++ src/main/java/com/api/apipeliculas/controllers/MovieController.java
@@ -0,0 +1,27 @@
+package com.api.apipeliculas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.api.apipeliculas.models.Movie;
+import com.api.apipeliculas.services.MovieService;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService service;
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return service.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable Long id) {
+        return service.getMovieById(id);
+    }
+}
